56c5212c76043fad392e5a2da347d8a0ca078f9d
nathaliebsl <75685745+nathaliebsl@users.noreply.github.com>
2021-01-13 17:38:18 -0300

    add pasta Funcionarios e add interface SistemaAutenticacao

--- javascript/javascript-oo/bytebank-js/Cliente.js
+++ javascript/javascript-oo/bytebank-js/Cliente.js
@@ -5,9 +5,15 @@ export class Cliente {
     }
 
     //boa prática é sempre declarar os atributos da classe dentro do construtor
-    constructor(nome, cpf) {
+    constructor(nome, cpf, senha) {
         this.nome =  nome;
         this._cpf = cpf; 
+        this._senha = senha;
+    }
+
+    //interface autenticar que permite que o Cliente acesse o SistemaAutenticacao.js e seja autenticável
+    autenticar(senha){
+        return senha == this._senha;
     }
 
 }
--- javascript/javascript-oo/bytebank-js/index.js
+++ javascript/javascript-oo/bytebank-js/index.js
@@ -1,20 +1,38 @@
 import { Cliente } from "./Cliente.js";
-import { ContaCorrente } from "./ContaCorrente.js";
-import { ContaPoupanca } from "./ContaPoupanca.js";
+import {Gerente} from "./Funcionario/Gerente.js"
+import {Diretor} from "./Funcionario/Diretor.js"
+import {SistemaAutenticacao} from "./SistemaAutenticacao.js"
 
-const cliente1 = new Cliente("Ricardo", 82702845053);
-const cliente2 = new Cliente("Alice", 11122233300);
+// import { ContaCorrente } from "./Conta/ContaCorrente.js";
+// import { ContaPoupanca } from "./Conta/ContaPoupanca.js";
 
-const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
-const contaPoupancaRicardo = new ContaPoupanca(50, cliente1, 1001);
+// const cliente1 = new Cliente("Ricardo", 82702845053);
+// const cliente2 = new Cliente("Alice", 11122233300);
 
-const conta2 = new ContaCorrente(cliente2, 1001);
+// const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
+// const contaPoupancaRicardo = new ContaPoupanca(50, cliente1, 1001);
 
-contaCorrenteRicardo.depositar(500);
+// const conta2 = new ContaCorrente(cliente2, 1001);
 
-contaCorrenteRicardo.transferir(200, conta2);
+// contaCorrenteRicardo.depositar(500);
 
-console.log(contaPoupancaRicardo);
+// contaCorrenteRicardo.transferir(200, conta2);
 
-console.log(contaCorrenteRicardo);
-console.log(conta2);
+// console.log(contaPoupancaRicardo);
+
+// console.log(contaCorrenteRicardo);
+// console.log(conta2);
+
+const diretor =  new Diretor("Rodrigo", 10000, 12345678900);
+diretor.cadastrarSenha("123456")
+const gerente =  new Gerente("Ricardo",  5000, 12378945601);
+gerente.cadastrarSenha("123");
+
+const cliente = new Cliente("Lais", 78945612379, "456");
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
+
+
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "456");
+
+console.log(gerenteEstaLogado, diretorEstaLogado, clienteEstaLogado);
