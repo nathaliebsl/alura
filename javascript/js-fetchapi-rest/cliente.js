c03f3d0db31cfd49049818a4437d1c1dff25ac4d
nathaliebsl <75685745+nathaliebsl@users.noreply.github.com>
2021-01-14 16:25:26 -0300

    add pasta server petshop

--- javascript/js-fetchapi-rest/cliente.js
+++ javascript/js-fetchapi-rest/cliente.js
@@ -1,7 +1,30 @@
-const infoClientes = [
-{
-    cpf: 82702845053,
-    nome: "Nathalie"
-}
-
-]
+const informacoesClientes = [
+    {
+      cpf: 82702845053,
+      nome: "Nathalie"
+    },
+    {
+      cpf: 12345678900,
+      nome: "Mathias"
+    },
+  ];
+  
+  const exibeCliente = (cpf, nome) => {
+    const linha = document.createElement("tr");
+    const conteudoLinha = 
+    // ` (crase) é a representação do Template literals
+    // ${} essa sintaxe permite que coloquemos uma expressão que tenha valor dentro. neste caso são duas variaveis cpf e nome, sendo uma int e uma string.
+    `
+    <td>${cpf}</td>
+    <td>${nome}</td>
+    `;
+  
+    linha.innerHTML = conteudoLinha;
+    return linha;
+  };
+  
+  const corpoTabela = document.querySelector("[data-conteudo-tabela]");
+  
+  informacoesClientes.forEach(indice => {
+    corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome))
+  })
