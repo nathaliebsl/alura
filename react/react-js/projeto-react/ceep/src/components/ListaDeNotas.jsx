568d730d5ac1278b9e0a0a3b0e12e034c58f92e3
nathaliebsl <75685745+nathaliebsl@users.noreply.github.com>
2021-01-25 18:24:28 -0300

    add clases App, CardNota, ListaDeNotas e FormularioCadastro

--- react/react-js/projeto-react/ceep/src/components/ListaDeNotas.jsx
+++ react/react-js/projeto-react/ceep/src/components/ListaDeNotas.jsx
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
+import CardNota from './CardNota';
 
-export default class ListaDeNotas extends Component{
+class ListaDeNotas extends Component{
 
     render(){
         return(
             <ul>
             <li>
-              <section>
-                <header>
-                  <h3>Título</h3>
-                </header>
-                <p>Escreva sua nota</p>
-              </section>
+              <CardNota></CardNota>
             </li>
+            <li>
+              <CardNota></CardNota>
+            </li>
+            <li>
+              <CardNota></CardNota>
+            </li>
+
           </ul>
         )
     }   
 
 }
+
+export default ListaDeNotas;
