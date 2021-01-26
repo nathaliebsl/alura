7b571174f23ee7143b815774866da1ed0d62cd34
nathaliebsl <75685745+nathaliebsl@users.noreply.github.com>
2021-01-26 12:30:30 -0300

    projeto organizado pastas assets, components

--- react/react-js/projeto-react/ceep/src/App.js
+++ react/react-js/projeto-react/ceep/src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from "react";
-import FormularioCadastro from "./components/FormularioCadastro";
 import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
 import "./assets/App.css";
-import "./assets/index.css";
+import './assets/index.css';
 
 
 class App extends Component {
   render(){
     return (
-      <section>
-        <FormularioCadastro></FormularioCadastro>
-        <ListaDeNotas></ListaDeNotas>
+      <section className="conteudo">
+        <FormularioCadastro />
+        <ListaDeNotas />
       </section>
     );
   }
