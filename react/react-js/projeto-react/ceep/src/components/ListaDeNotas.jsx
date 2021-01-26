bb8ca73f0000e11b28c80ed8bdf84e05f42121e6
nathaliebsl <75685745+nathaliebsl@users.noreply.github.com>
2021-01-26 10:16:10 -0300

    add css App, index e  estilo CardNota

--- react/react-js/projeto-react/ceep/src/components/ListaDeNotas.jsx
+++ react/react-js/projeto-react/ceep/src/components/ListaDeNotas.jsx
@@ -1,25 +1,21 @@
-import React, { Component } from 'react';
-import CardNota from './CardNota';
+import React, { Component } from "react";
+import CardNota from "./CardNota/CardNota";
 
-class ListaDeNotas extends Component{
-
-    render(){
-        return(
-            <ul>
-            <li>
+class ListaDeNotas extends Component {
+  render() {
+    return (
+      <ul>
+        {Array.of("work", "work", "study").map((categoria, index) => {
+          return (
+            <li key={index}>
+              <div>{categoria}</div>
               <CardNota></CardNota>
             </li>
-            <li>
-              <CardNota></CardNota>
-            </li>
-            <li>
-              <CardNota></CardNota>
-            </li>
-
-          </ul>
-        )
-    }   
-
+          );
+        })}
+      </ul>
+    );
+  }
 }
 
 export default ListaDeNotas;
