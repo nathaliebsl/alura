15723918c652e4c4bc9176126d81ba42303749e0
nathaliebsl <75685745+nathaliebsl@users.noreply.github.com>
2021-01-26 18:44:51 -0300

    finalizando projeto ceed

--- react/react-js/projeto-react/ceep/src/App.js
+++ react/react-js/projeto-react/ceep/src/App.js
@@ -2,15 +2,30 @@ import React, { Component } from "react";
 import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
 import "./assets/App.css";
-import './assets/index.css';
-
+import "./assets/index.css";
 
 class App extends Component {
-  render(){
+  constructor() {
+    super();
+    this.state = {
+      notas: [],
+    };
+  }
+
+  criarNota(titulo, texto) {
+    const novaNota = { titulo, texto };
+    const novoArrayNotas = [...this.state.notas, novaNota];
+    const novoEstado = {
+      notas: novoArrayNotas,
+    };
+    this.setState(novoEstado);
+  }
+
+  render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro />
-        <ListaDeNotas />
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.state.notas} />
       </section>
     );
   }
--- react/react-js/projeto-react/ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
+++ react/react-js/projeto-react/ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -6,11 +6,10 @@ class ListaDeNotas extends Component {
   render() {
     return (
       <ul className="lista-notas">
-        {Array.of("work", "work", "study").map((categoria, index) => {
+        {this.props.notas.map((nota, index) => {
           return (
             <li className="lista-notas_item" key={index}>
-              <div>{categoria}</div>
-              <CardNota></CardNota>
+              <CardNota titulo={nota.titulo} texto={nota.texto} />
             </li>
           );
         })}
