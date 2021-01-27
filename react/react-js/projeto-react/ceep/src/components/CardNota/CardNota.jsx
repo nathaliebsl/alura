984237aa808d574e01b6651545b1322e25de0580
nathaliebsl <75685745+nathaliebsl@users.noreply.github.com>
2021-01-27 19:08:09 -0300

    add evento para add categoria na nova nota

--- react/react-js/projeto-react/ceep/src/components/CardNota/CardNota.jsx
+++ react/react-js/projeto-react/ceep/src/components/CardNota/CardNota.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 import "./estilo.css";
-import deleteSVG from '../../assets/img/delete.svg'
+import deleteSVG from "../../assets/img/delete.svg";
 
 class CardNota extends Component {
-
-  delete(){
+  delete() {
     const indice = this.props.indice;
     this.props.apagarNota(indice);
   }
@@ -14,9 +13,12 @@ class CardNota extends Component {
       <section className="card-nota">
         <header className="card-nota_cabecalho">
           <h3 className="card-nota_titulo">{this.props.titulo}</h3>
-          <img src={deleteSVG} 
-          onClick={this.delete.bind(this)}
-          alt="trash bin"/>
+          <img
+            src={deleteSVG}
+            onClick={this.delete.bind(this)}
+            alt="trash bin"
+          />
+          <h4>{this.props.categoria}</h4>
         </header>
         <p className="card-nota_texto">{this.props.texto}</p>
       </section>
