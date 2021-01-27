a3abae1cfd62c6aacd590cb28ef7551ce004033b
nathaliebsl <75685745+nathaliebsl@users.noreply.github.com>
2021-01-27 11:18:17 -0300

    add funcao deletarNota

--- react/react-js/projeto-react/ceep/src/components/CardNota/CardNota.jsx
+++ react/react-js/projeto-react/ceep/src/components/CardNota/CardNota.jsx
@@ -3,12 +3,20 @@ import "./estilo.css";
 import deleteSVG from '../../assets/img/delete.svg'
 
 class CardNota extends Component {
+
+  delete(){
+    const indice = this.props.indice;
+    this.props.apagarNota(indice);
+  }
+
   render() {
     return (
       <section className="card-nota">
         <header className="card-nota_cabecalho">
           <h3 className="card-nota_titulo">{this.props.titulo}</h3>
-          <img src={deleteSVG} />
+          <img src={deleteSVG} 
+          onClick={this.delete.bind(this)}
+          alt="trash bin"/>
         </header>
         <p className="card-nota_texto">{this.props.texto}</p>
       </section>
