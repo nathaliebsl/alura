51903ea67c1affe13177dc1ca50a0ef2a2da9697
nathaliebsl <75685745+nathaliebsl@users.noreply.github.com>
2021-01-29 16:32:48 -0300

    add Hook useErros e finalizacao do projeto da aula de Hooks e Contextos da Alura

--- react/react-js/projeto-cadastro/fcreactjs/src/components/FormularioCadastro/DadosUsuario.jsx
+++ react/react-js/projeto-cadastro/fcreactjs/src/components/FormularioCadastro/DadosUsuario.jsx
@@ -1,29 +1,13 @@
 import { Button, TextField } from "@material-ui/core";
 import React, { useState, useContext } from "react";
 import ValidacoesCadastro from "../../context/ValidacoesCadastro";
+import useErros from "../../hooks/useErros"
 
 function DadosUsuario({ aoEnviar }) {
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
-  const [erros, setErros] = useState({ senha: { valido: true, texto: "" } });
-
   const validacoes = useContext(ValidacoesCadastro);
-
-  function validarCampos(event) {
-    const { name, value } = event.target;
-    const novoEstado = { ...erros };
-    novoEstado[name] = validacoes[name](value);
-    setErros(novoEstado);
-  }
-
-  function possoEnviar() {
-    for (let campo in erros) {
-      if (!erros[campo].valido) {
-        return false;
-      }
-    }
-    return true;
-  }
+  const [erros, validarCampos, possoEnviar] = useErros(validacoes);
 
   return (
     <form
